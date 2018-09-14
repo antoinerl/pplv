9916155e7016cf6d06fe247f44ca5238ee26400f
antoine <antoine@4c530cca-dbec-42c6-82d8-454c67d29713>
2018-09-14 14:00:10 +0000

    inscription

--- components/calendar/calendar.ts
+++ components/calendar/calendar.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CalendarComponentOptions, DayConfig } from 'ion2-calendar';
 import * as moment from 'moment';
 import { RecurrentMenuComponent } from '../recurrent-menu/recurrent-menu';
@@ -16,7 +16,6 @@ import { DateProvider } from '../../providers/date/date';
   templateUrl: 'calendar.html',
 })
 export class CalendarComponent {
-  @ViewChild('iframe') iframe: ElementRef;
 
   selectedDate: number;
   idUser: number;
@@ -120,9 +119,7 @@ export class CalendarComponent {
     }
 
     displayAlert(data) {
-      let win =  this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentWindow;
-      alert(win);
-      win.postMessage(data, "*");
+      parent.postMessage(data, "*");
     }
 
 }
